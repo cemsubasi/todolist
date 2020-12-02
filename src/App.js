9a158ef2d175f3af3068e05182904caff640d734
cemsubasi <cemsubasi@live.com>
2020-12-03 01:48:26 +0300

    localStorage added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import Users from './Users';
 import './App.css';
 
@@ -7,12 +7,15 @@ export default function App() {
 	const userList = [
 	];
 
-	const [users, setUsers] = useState(userList);
+	const localItems = JSON.parse(localStorage.getItem("localItems"));
+	const [users, setUsers] = useState(localItems || userList);
 	const userMapping = user => setUsers(users.map(us => user.id === us.id ? {...us, done: !us.done} : us));
 	const addToList = user => setUsers([...users, {id: Date.now(), name: user, done: false}]);
 	const userDelete = user => setUsers(users.filter(us => !us.done ? [...user] : null));
 	const [input, setInput] = useState("");
 	const addInput = e => setInput(e.target.value);
+
+	useEffect(() => localStorage.setItem("localItems", JSON.stringify(users)),[users]);
 	
     return (
 	    <div className="container">
